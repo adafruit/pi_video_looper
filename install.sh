ac7864d56317b270e1edaad1d3efbad9b45024ae
Tobias Perschon <tofuSCHNITZEL@users.noreply.github.com>
2019-08-24 10:40:46 +0200

    Update install.sh
    fixed pi_hello_video url

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ then
 	echo "Installing hello_video..."
 	echo "========================="
 	apt -y install git build-essential python3-dev
-	git clone https://github.com/adafruit/pi_hello_video.gi
+	git clone https://github.com/adafruit/pi_hello_video
 	cd pi_hello_video
 	./rebuild.sh
 	cd hello_video
