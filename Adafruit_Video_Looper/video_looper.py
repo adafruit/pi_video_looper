479b1d3c4ca92fc6763b5429f1fba11739fe0ae2
Tobias Perschon <tobias@perschon.at>
2021-08-09 20:15:51 +0200

    v1.0.8
     - playlist resume option
       when enabled will resume last played file on restart
     - console output now has a timestamp for easier event tracking
     - Keyboard key for shutdown added ("p")

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -110,7 +110,7 @@ class VideoLooper:
         """Print message to standard output if console output is enabled."""
         if self._console_output:
             now = datetime.now()
-            print("[{}] {}".format(now,message))
+            print("[{}] {}".format(now, message))
 
     def _load_player(self):
         """Load the configured video player and return an instance of it."""
@@ -196,7 +196,7 @@ class VideoLooper:
 
             for x in os.listdir(path):
                 # Ignore hidden files (useful when file loaded on usb key from an OSX computer
-                if x[0] is not '.' and re.search('\.({0})$'.format(self._extensions), x, flags=re.IGNORECASE):
+                if x[0] != '.' and re.search('\.({0})$'.format(self._extensions), x, flags=re.IGNORECASE):
                     repeatsetting = re.search('_repeat_([0-9]*)x', x, flags=re.IGNORECASE)
                     if (repeatsetting is not None):
                         repeat = repeatsetting.group(1)
