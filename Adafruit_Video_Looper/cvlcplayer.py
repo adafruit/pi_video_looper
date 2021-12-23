7705603050c9ca972051ff050837da8a93bc64b4
Tobias Perschon <tobias@perschon.at>
2021-12-23 14:06:08 +0100

    WIP: basic playback functionality

--- Adafruit_Video_Looper/cvlcplayer.py
+++ Adafruit_Video_Looper/cvlcplayer.py
@@ -32,21 +32,21 @@ class CVLCPlayer:
         self._extensions = config.get('cvlc', 'extensions') \
                                  .translate(str.maketrans('', '', ' \t\r\n.')) \
                                  .split(',')
-        self._extra_args = config.get('cvlc', 'extra_args').split()
-        self._sound = config.get('cvlc', 'sound').lower()
-        assert self._sound in ('hdmi', 'local', 'both', 'alsa'), 'Unknown cvlc sound configuration value: {0} Expected hdmi, local, both or alsa.'.format(self._sound)
-        self._alsa_hw_device = parse_hw_device(config.get('alsa', 'hw_device'))
-        if self._alsa_hw_device != None and self._sound == 'alsa':
-            self._sound = 'alsa:hw:{},{}'.format(self._alsa_hw_device[0], self._alsa_hw_device[1])
-        self._show_titles = config.getboolean('cvlc', 'show_titles')
-        if self._show_titles:
-            title_duration = config.getint('cvlc', 'title_duration')
-            if title_duration >= 0:
-                m, s = divmod(title_duration, 60)
-                h, m = divmod(m, 60)
-                self._subtitle_header = '00:00:00,00 --> {:d}:{:02d}:{:02d},00\n'.format(h, m, s)
-            else:
-                self._subtitle_header = '00:00:00,00 --> 99:59:59,00\n'
+        #self._extra_args = config.get('cvlc', 'extra_args').split()
+        #self._sound = config.get('cvlc', 'sound').lower()
+        #assert self._sound in ('hdmi', 'local', 'both', 'alsa'), 'Unknown cvlc sound configuration value: {0} Expected hdmi, local, both or alsa.'.format(self._sound)
+        #self._alsa_hw_device = parse_hw_device(config.get('alsa', 'hw_device'))
+        #if self._alsa_hw_device != None and self._sound == 'alsa':
+        #    self._sound = 'alsa:hw:{},{}'.format(self._alsa_hw_device[0], self._alsa_hw_device[1])
+        #self._show_titles = config.getboolean('cvlc', 'show_titles')
+        #if self._show_titles:
+        #    title_duration = config.getint('cvlc', 'title_duration')
+        #    if title_duration >= 0:
+        #        m, s = divmod(title_duration, 60)
+        #        h, m = divmod(m, 60)
+        #        self._subtitle_header = '00:00:00,00 --> {:d}:{:02d}:{:02d},00\n'.format(h, m, s)
+        #    else:
+        #        self._subtitle_header = '00:00:00,00 --> 99:59:59,00\n'
 
     def supported_extensions(self):
         """Return list of supported file extensions."""
@@ -56,9 +56,9 @@ class CVLCPlayer:
         """Play the provided movie file, optionally looping it repeatedly."""
         self.stop(3)  # Up to 3 second delay to let the old player stop.
         # Assemble list of arguments.
-        args = ['cvlc']
+        args = ['sudo', '-u','pi', 'cvlc']
         #args.extend(['-o', self._sound])  # Add sound arguments.
-        args.extend(self._extra_args)     # Add extra arguments from config.
+        #args.extend(self._extra_args)     # Add extra arguments from config.
         #if vol is not 0:
         #    args.extend(['--vol', str(vol)])
         if loop is None:
@@ -67,14 +67,14 @@ class CVLCPlayer:
             args.append('--loop')  # Add loop parameter if necessary.
         else:
             args.append('--play-and-exit')
-        if self._show_titles and movie.title:
-            srt_path = os.path.join(self._get_temp_directory(), 'video_looper.srt')
-            with open(srt_path, 'w') as f:
-                f.write(self._subtitle_header)
-                f.write(movie.title)
-            args.extend(['--subtitles', srt_path])
-        else:
-            args.append('--no-osd')
+        #if self._show_titles and movie.title:
+        #    srt_path = os.path.join(self._get_temp_directory(), 'video_looper.srt')
+        #    with open(srt_path, 'w') as f:
+        #        f.write(self._subtitle_header)
+        #        f.write(movie.title)
+        #    args.extend(['--subtitles', srt_path])
+        #else:
+        #    args.append('--no-osd')
         args.append(movie.filename)       # Add movie file path.
         # Run cvlc process and direct standard output to /dev/null.
         self._process = subprocess.Popen(args,
@@ -96,7 +96,7 @@ class CVLCPlayer:
         if self._process is not None and self._process.returncode is None:
             # There are a couple processes used by cvlc, so kill both
             # with a pkill command.
-            subprocess.call(['pkill', '-9', 'cvlc'])
+            subprocess.call(['pkill', '-9', 'vlc'])
         # If a blocking timeout was specified, wait up to that amount of time
         # for the process to stop.
         start = time.time()
