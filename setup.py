89dc2ff759b61274b978008edac0551c2edd60fe
Tobias Perschon <tobias@perschon.at>
2022-02-01 17:40:50 +0100

    fix dependency issue

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name              = 'Adafruit_Video_Looper',
       description       = 'Application to turn your Raspberry Pi into a dedicated looping video playback device, good for art installations, information displays, or just playing cat videos all day.',
       license           = 'GNU GPLv2',
       url               = 'https://github.com/adafruit/pi_video_looper',
-      install_requires  = ['pyudev', 'pygame'],
+      install_requires  = ['pyudev>=0.23.1', 'pygame', 'six>=1.16.0'],
       packages          = find_packages())
